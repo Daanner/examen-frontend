ff565a1c1fa9d44f7bd0710c22fd926e3620f33e
kevin flores <kevindannerflores@gmail.com>
2025-10-27 21:52:19 -0400

    feat(web): crear servicio empleados y consumir API

---
+++ src/app/services/empleado.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class EmpleadoService {
+  private apiUrl = 'http://127.0.0.1:8000/api//empleados';
+
+  constructor(private http: HttpClient) { }
+
+
+  listaEmpleados():Observable<any[]>{
+  return this.http.get<any[]>(this.base);
+  }
+
+ crearEmpleado(producto: any): Observable<any> {
+    return this.http.post<any>(this.base, producto);
+  }
+
+}
